bab1f615f5d1532ff7524e7da4cdd8e7d2ed8d1d
Intemporelle <contact@intemporelle.eu>
2026-05-10 00:50:32 +0000

    chore: base propre Intemporelle - nettoyage studios test + nginx port 3002 - 2026-05-10

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -93,3 +93,4 @@ document.addEventListener('input', (e) => {
 // Force rebuild Fri Apr  3 04:58:19 UTC 2026
 // Force update iPad/PC - Sun May 10 00:16:40 UTC 2026
 // Force update SIRET - Sun May 10 00:32:59 UTC 2026
+// Base propre Intemporelle - Sun May 10 00:46:35 UTC 2026
